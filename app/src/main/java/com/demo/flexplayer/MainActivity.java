c1b53c360fa51dc1971333865a41d8edd789617e
winson <929566854@qq.com>
2018-12-05 16:12:57 +0800

    update test;

--- app/src/main/java/com/demo/flexplayer/MainActivity.java
+++ app/src/main/java/com/demo/flexplayer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.demo.flexplayer;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.winson.flexplayer.FlexPlayerView;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     String TEST_PATH2 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/testmp4.mp4";
     String TEST_PATH3 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/apeng/test.mp4";
     String TEST_PATH4 = "https://dakaimg.ciweilive.com/erp_114C13721DB0A5B4D8DFE7AB71A48026.mp4";
+    String TEST_PATH5 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/document/video:14";
 
     @Override
     public void onBackPressed() {
@@ -26,12 +29,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     FlexPlayerView flexPlayerView;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        videoView = findViewById(R.id.video_view);
         flexPlayerView = findViewById(R.id.flex_player);
         Button play = findViewById(R.id.http_path);
 
@@ -54,15 +58,20 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.size).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flexPlayerView.setUp(v.getContext(), TEST_PATH3);
+//                flexPlayerView.setUp(v.getContext(), TEST_PATH3);
+
+//                videoView.setVideoURI(Uri.parse(TEST_PATH4));
+                videoView.setVideoPath(TEST_PATH5);
+                videoView.start();
+
             }
         });
 
         flexPlayerView.setUp(this, TEST_PATH4);
 //        flexPlayerView.setUp(this, TEST_PATH);
-        flexPlayerView.start();
-        flexPlayerView.enterFullScreen();
-        flexPlayerView.getController().showBackImage(true);
+//        flexPlayerView.start();
+//        flexPlayerView.enterFullScreen();
+//        flexPlayerView.getController().showBackImage(true);
 
 //        flexPlayerView.setContainerBackground(getResources().getDrawable(R.drawable.white_background));
 
